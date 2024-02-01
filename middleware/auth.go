6c71e1741cc91928e9bc0ad5b4a4d829eb2e9755
jayyi <jayyi@tencent.com>
2024-02-02 07:00:44 +0800

    Add debug logs

--- middleware/auth.go
+++ middleware/auth.go
@@ -3,99 +3,100 @@
 package middleware
 
 import (
-	"context"
-	"io"
-	"net/http"
-	"strings"
+    "context"
+    "io"
+    "net/http"
+    "strings"
 )
 import (
-	"github.com/zeromicro/go-zero/core/logc"
-	"github.com/zeromicro/go-zero/core/logx"
-	"github.com/zeromicro/go-zero/zrpc"
+    "github.com/zeromicro/go-zero/core/logc"
+    "github.com/zeromicro/go-zero/core/logx"
+    "github.com/zeromicro/go-zero/zrpc"
 )
 import (
-	"mooon-gateway/mooonauth"
-	"mooon-gateway/pb/mooon_auth"
+    "mooon-gateway/mooonauth"
+    "mooon-gateway/pb/mooon_auth"
 )
 
 // AuthMiddleware 鉴权
 func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		logCtx := logx.ContextWithFields(r.Context(), logx.Field("path", r.URL.Path))
+    return func(w http.ResponseWriter, r *http.Request) {
+        logCtx := logx.ContextWithFields(r.Context(), logx.Field("path", r.URL.Path))
 
-		if !strings.HasPrefix(r.URL.Path, GlobalConfig.Auth.Prefix) {
-			next.ServeHTTP(w, r)
-		} else {
-			var authReq mooon_auth.AuthReq
+        logc.Debugf(logCtx, "Auth.Prefix: %s, r.URL.Path: %s\n", GlobalConfig.Auth.Prefix, r.URL.Path)
+        if !strings.HasPrefix(r.URL.Path, GlobalConfig.Auth.Prefix) {
+            next.ServeHTTP(w, r)
+        } else {
+            var authReq mooon_auth.AuthReq
 
-			mooonAuth, err := getAuthClient(logCtx)
-			if err != nil {
-				responseBytes, err := NewResponseStr(logCtx, GwErrConnAuth, "conn auth error", nil)
-				if err == nil {
-					w.Header().Set("Content-Type", "application/json")
-					w.Write(responseBytes)
-				}
-				return
-			}
+            mooonAuth, err := getAuthClient(logCtx)
+            if err != nil {
+                responseBytes, err := NewResponseStr(logCtx, GwErrConnAuth, "conn auth error", nil)
+                if err == nil {
+                    w.Header().Set("Content-Type", "application/json")
+                    w.Write(responseBytes)
+                }
+                return
+            }
 
-			reqBodyBytes, err := io.ReadAll(r.Body)
-			if err != nil {
-				logc.Errorf(logCtx, "Read request body error: %s\n", err.Error())
-				return
-			}
-			defer r.Body.Close()
+            reqBodyBytes, err := io.ReadAll(r.Body)
+            if err != nil {
+                logc.Errorf(logCtx, "Read request body error: %s\n", err.Error())
+                return
+            }
+            defer r.Body.Close()
 
-			authReq.Body = reqBodyBytes
-			authResp, err := mooonAuth.Authenticate(r.Context(), &authReq)
-			if err != nil {
-				logc.Errorf(logCtx, "Call auth failed: %s\n", err.Error())
-				responseBytes, err := NewResponseStr(logCtx, GwErrCallAuth, "call auth error", nil)
-				if err == nil {
-					w.Write(responseBytes)
-					return
-				}
-			}
+            authReq.Body = reqBodyBytes
+            authResp, err := mooonAuth.Authenticate(r.Context(), &authReq)
+            if err != nil {
+                logc.Errorf(logCtx, "Call auth failed: %s\n", err.Error())
+                responseBytes, err := NewResponseStr(logCtx, GwErrCallAuth, "call auth error", nil)
+                if err == nil {
+                    w.Write(responseBytes)
+                    return
+                }
+            }
 
-			// 写 http 头
-			for name, value := range authResp.HttpHeaders {
-				w.Header().Set(name, value)
-			}
-			// 写 cookies
-			for _, authCookie := range authResp.HttpCookies {
-				httpCookie := AuthCookie2HttpCookie(authCookie)
-				http.SetCookie(w, httpCookie)
-			}
-			// 写响应体
-			if len(authResp.Body) > 0 {
-				responseBytes, err := NewResponseStr(logCtx, GwSuccess, "", authResp.Body)
-				if err == nil {
-					_, err = w.Write(responseBytes) // 得放在最后
-					if err != nil {
-						logc.Errorf(logCtx, "Write response: %s\n", err.Error())
-					} else {
-						w.WriteHeader(http.StatusOK)
-					}
-				}
-			}
-		}
-	}
+            // 写 http 头
+            for name, value := range authResp.HttpHeaders {
+                w.Header().Set(name, value)
+            }
+            // 写 cookies
+            for _, authCookie := range authResp.HttpCookies {
+                httpCookie := AuthCookie2HttpCookie(authCookie)
+                http.SetCookie(w, httpCookie)
+            }
+            // 写响应体
+            if len(authResp.Body) > 0 {
+                responseBytes, err := NewResponseStr(logCtx, GwSuccess, "", authResp.Body)
+                if err == nil {
+                    _, err = w.Write(responseBytes) // 得放在最后
+                    if err != nil {
+                        logc.Errorf(logCtx, "Write response: %s\n", err.Error())
+                    } else {
+                        w.WriteHeader(http.StatusOK)
+                    }
+                }
+            }
+        }
+    }
 }
 
 func getAuthClient(logCtx context.Context) (mooonauth.MooonAuth, error) {
-	/*var authConf zrpc.RpcClientConf
+    /*var authConf zrpc.RpcClientConf
 
-	  err := conf.Load("etc/auth.yaml", &authConf)
-	  if err != nil {
-	  	logc.Errorf(logCtx, "Load conf error: %s\n", err.Error())
-	  	return nil, err
-	  }*/
+      err := conf.Load("etc/auth.yaml", &authConf)
+      if err != nil {
+      	logc.Errorf(logCtx, "Load conf error: %s\n", err.Error())
+      	return nil, err
+      }*/
 
-	zrpcClient, err := zrpc.NewClient(GlobalConfig.Auth.RpcClientConf)
-	if err != nil {
-		logc.Errorf(logCtx, "New auth client error: %s\n", err.Error())
-		return nil, err
-	}
+    zrpcClient, err := zrpc.NewClient(GlobalConfig.Auth.RpcClientConf)
+    if err != nil {
+        logc.Errorf(logCtx, "New auth client error: %s\n", err.Error())
+        return nil, err
+    }
 
-	authClient := mooonauth.NewMooonAuth(zrpcClient)
-	return authClient, nil
+    authClient := mooonauth.NewMooonAuth(zrpcClient)
+    return authClient, nil
 }
--- middleware/login.go
+++ middleware/login.go
@@ -3,100 +3,101 @@
 package middleware
 
 import (
-	"context"
-	"io"
-	"net/http"
-	"strings"
+    "context"
+    "io"
+    "net/http"
+    "strings"
 )
 import (
-	"github.com/zeromicro/go-zero/core/logc"
-	"github.com/zeromicro/go-zero/core/logx"
-	"github.com/zeromicro/go-zero/zrpc"
+    "github.com/zeromicro/go-zero/core/logc"
+    "github.com/zeromicro/go-zero/core/logx"
+    "github.com/zeromicro/go-zero/zrpc"
 )
 import (
-	"mooon-gateway/mooonlogin"
-	"mooon-gateway/pb/mooon_login"
+    "mooon-gateway/mooonlogin"
+    "mooon-gateway/pb/mooon_login"
 )
 
 // LoginMiddleware 登录
 func LoginMiddleware(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		logCtx := logx.ContextWithFields(r.Context(), logx.Field("path", r.URL.Path))
+    return func(w http.ResponseWriter, r *http.Request) {
+        logCtx := logx.ContextWithFields(r.Context(), logx.Field("path", r.URL.Path))
 
-		if !strings.HasPrefix(r.URL.Path, GlobalConfig.Login.Prefix) {
-			next.ServeHTTP(w, r)
-		} else {
-			var loginReq mooon_login.LoginReq
+        logc.Debugf(logCtx, "Login.Prefix: %s, r.URL.Path: %s\n", GlobalConfig.Login.Prefix, r.URL.Path)
+        if !strings.HasPrefix(r.URL.Path, GlobalConfig.Login.Prefix) {
+            next.ServeHTTP(w, r)
+        } else {
+            var loginReq mooon_login.LoginReq
 
-			mooonLogin, err := getLoginClient(logCtx)
-			if err != nil {
-				responseBytes, err := NewResponseStr(logCtx, GwErrConnLogin, "conn login error", nil)
-				if err == nil {
-					w.Header().Set("Content-Type", "application/json")
-					w.Write(responseBytes)
-				}
-				return
-			}
+            mooonLogin, err := getLoginClient(logCtx)
+            if err != nil {
+                responseBytes, err := NewResponseStr(logCtx, GwErrConnLogin, "conn login error", nil)
+                if err == nil {
+                    w.Header().Set("Content-Type", "application/json")
+                    w.Write(responseBytes)
+                }
+                return
+            }
 
-			reqBodyBytes, err := io.ReadAll(r.Body)
-			if err != nil {
-				logc.Errorf(logCtx, "Read request body error: %s\n", err.Error())
-				return
-			}
-			defer r.Body.Close()
+            reqBodyBytes, err := io.ReadAll(r.Body)
+            if err != nil {
+                logc.Errorf(logCtx, "Read request body error: %s\n", err.Error())
+                return
+            }
+            defer r.Body.Close()
 
-			loginReq.Body = reqBodyBytes
-			loginResp, err := mooonLogin.Login(r.Context(), &loginReq)
-			if err != nil {
-				logc.Errorf(logCtx, "Call login failed: %s\n", err.Error())
-				responseBytes, err := NewResponseStr(logCtx, GwErrCallLogin, "call login error", nil)
-				if err == nil {
-					w.Header().Set("Content-Type", "application/json")
-					w.Write(responseBytes)
-				}
-				return
-			}
+            loginReq.Body = reqBodyBytes
+            loginResp, err := mooonLogin.Login(r.Context(), &loginReq)
+            if err != nil {
+                logc.Errorf(logCtx, "Call login failed: %s\n", err.Error())
+                responseBytes, err := NewResponseStr(logCtx, GwErrCallLogin, "call login error", nil)
+                if err == nil {
+                    w.Header().Set("Content-Type", "application/json")
+                    w.Write(responseBytes)
+                }
+                return
+            }
 
-			// 写 http 头
-			for name, value := range loginResp.HttpHeaders {
-				w.Header().Set(name, value)
-			}
-			// 写 cookies
-			for _, loginCookie := range loginResp.HttpCookies {
-				httpCookie := LoginCookie2HttpCookie(loginCookie)
-				http.SetCookie(w, httpCookie)
-			}
-			// 写响应体
-			if len(loginResp.Body) > 0 {
-				responseBytes, err := NewResponseStr(logCtx, GwSuccess, "", loginResp.Body)
-				if err == nil {
-					_, err = w.Write(responseBytes) // 得放在最后
-					if err != nil {
-						logc.Errorf(logCtx, "Write response: %s\n", err.Error())
-					} else {
-						w.WriteHeader(http.StatusOK)
-					}
-				}
-			}
-		}
-	}
+            // 写 http 头
+            for name, value := range loginResp.HttpHeaders {
+                w.Header().Set(name, value)
+            }
+            // 写 cookies
+            for _, loginCookie := range loginResp.HttpCookies {
+                httpCookie := LoginCookie2HttpCookie(loginCookie)
+                http.SetCookie(w, httpCookie)
+            }
+            // 写响应体
+            if len(loginResp.Body) > 0 {
+                responseBytes, err := NewResponseStr(logCtx, GwSuccess, "", loginResp.Body)
+                if err == nil {
+                    _, err = w.Write(responseBytes) // 得放在最后
+                    if err != nil {
+                        logc.Errorf(logCtx, "Write response: %s\n", err.Error())
+                    } else {
+                        w.WriteHeader(http.StatusOK)
+                    }
+                }
+            }
+        }
+    }
 }
 
 func getLoginClient(logCtx context.Context) (mooonlogin.MooonLogin, error) {
-	/*var loginConf zrpc.RpcClientConf
+    /*var loginConf zrpc.RpcClientConf
 
-	  err := conf.Load("etc/login.yaml", &loginConf)
-	  if err != nil {
-	  	logc.Errorf(logCtx, "Load conf error: %s\n", err.Error())
-	  	return nil, err
-	  }*/
+      err := conf.Load("etc/login.yaml", &loginConf)
+      if err != nil {
+      	logc.Errorf(logCtx, "Load conf error: %s\n", err.Error())
+      	return nil, err
+      }*/
 
-	zrpcClient, err := zrpc.NewClient(GlobalConfig.Login.RpcClientConf)
-	if err != nil {
-		logc.Errorf(logCtx, "New login client error: %s\n", err.Error())
-		return nil, err
-	}
+    zrpcClient, err := zrpc.NewClient(GlobalConfig.Login.RpcClientConf)
+    if err != nil {
+        logc.Errorf(logCtx, "New login client error: %s\n", err.Error())
+        return nil, err
+    }
 
-	loginClient := mooonlogin.NewMooonLogin(zrpcClient)
-	return loginClient, nil
+    loginClient := mooonlogin.NewMooonLogin(zrpcClient)
+    return loginClient, nil
 }
